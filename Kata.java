32ceb450995e241dce073f77f33063484688079b
Cameron Thacker <cthacker@udel.edu>
2022-02-16 04:55:21 -0500

    [master] Added functions

--- Kata.java
+++ Kata.java
@@ -44,9 +44,57 @@ public class Kata {
 
     }
 
+    public static int[] smaller(int[] unsorted) {
+
+        int count = 0;
+
+        for (int i = unsorted.length - 1; i >= 0; i--) {
+            if (i <= unsorted.length - 2 && unsorted[i] > unsorted[i + 1]) {
+                unsorted[i] = ++count;
+            } else {
+                unsorted[i] = count;
+            }
+        }
+        return unsorted;
+
+    }
+
+    public static int[] waveSort(int[] array) {
+
+        Arrays.sort(array);
+        for (int i = 0; i < array.length - 1; i += 2) {
+            int tmp = array[i + 1];
+            array[i + 1] = array[i];
+            array[i] = tmp;
+        }
+        return array;
+
+    }
+
+    public static String meeting(String s) {
+
+        List<String> people = Stream.of(s.split(";")).collect(Collectors.toList());
+        Collections.sort(people, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                String[] names_o1 = o1.split(":");
+                String[] names_o2 = o2.split(":");
+                if (names_o1[1].toUpperCase().equals(names_o2[1].toUpperCase())) {
+                    // same last names
+                    return names_o1[0].toUpperCase().compareTo(names_o2[0].toUpperCase());
+                }
+                return names_o1[1].toUpperCase().compareTo(names_o2[1].toUpperCase());
+            }
+            
+        });
+        return people.stream().map(e -> String.format("(%s, %s)", e.split(":")[1].toUpperCase(), e.split(":")[0].toUpperCase())).collect(Collectors.joining(""));
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(sortArray(new int[]{5, 8, 6, 3, 4})));
+        System.out.println(Arrays.toString(waveSort(new int[] {1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4})));
 
     }
 
